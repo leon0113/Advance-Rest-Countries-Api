ecdbeb63f356bdd25c34002dcea78e831323740f
leon0113 <tahjib.leon399@gmail.com>
2023-02-03 20:21:48 +0600

    separated web apge for each country

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 
 const Country = () => {
+    const { name } = useParams();
+
+    const [country, setCountry] = useState([]);
+    useEffect(() => {
+        fetch(`https://restcountries.com/v3.1/name/${name}`)
+            .then(res => res.json())
+            .then(data => setCountry(data));
+    }, []);
     return (
         <div>
-
+            <section>
+                {
+                    country.map(item =>
+                    (
+                        <div>
+                            <article>
+                                <img src={item.flags.svg} alt="" />
+                                <h1>{item.name.common}</h1>
+                            </article>
+                        </div>
+                    )
+                    )
+                }
+            </section>
         </div>
     );
 };
