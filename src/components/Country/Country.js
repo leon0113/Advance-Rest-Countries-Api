8fa31dd83e4317d11f07973c1170aeec3873f172
leon0113 <tahjib.leon399@gmail.com>
2023-02-05 00:29:28 +0600

    designing country page

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -13,17 +13,34 @@ const Country = () => {
     }, []);
     return (
         <div>
-            <section>
+            <section className='p-8 md:py-0 max-w-7xl mx-auto'>
                 {
                     country.map(item =>
                     (
-                        <div key={item.population}>
+                        <div key={item.population} className="grid grid-col-1 gap-8 md:grid-cols-2 md:place-items-center md:h-screen">
                             <article>
                                 <img src={item.flags.svg} alt={item.name.common} />
                             </article>
 
                             <article>
-                                <h1>{item.name.official}</h1>
+                                <h1 className='font-bold text-gray-900 text-4xl lg:text-5xl'>{item.name.official}</h1>
+                                <ul className='mt-4 flex flex-col items-start justify-start gap-2 text-slate-700'>
+                                    <li>Capital : {item.capital[0]}</li>
+                                    <li>Population : {item.population}</li>
+                                    {/* <li>Currency :</li> */}
+                                    <li>Region : {item.region}</li>
+                                    <li>Subregion : {item.subregion}</li>
+                                </ul>
+                                {item.borders && <div>
+                                    <h3>Borders :</h3>
+                                    <ul>
+                                        {
+                                            item.borders.map((border, index) => (
+                                                <li key={index}>{border}</li>
+                                            ))
+                                        }
+                                    </ul>
+                                </div>}
                             </article>
                         </div>
                     )
