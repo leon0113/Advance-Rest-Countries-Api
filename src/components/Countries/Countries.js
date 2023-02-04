f8ed7a5b81479e33b022d521c96e979535fea010
leon0113 <tahjib.leon399@gmail.com>
2023-02-04 23:53:11 +0600

    save bandwith

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -9,7 +9,7 @@ const Countries = () => {
             try {
                 const res = await fetch("https://restcountries.com/v3.1/all");
                 const data = await res.json();
-                setCountries(data);
+                setCountries(data.slice(0, 12));
             } catch (error) {
                 console.error(error)
             }
