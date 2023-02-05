1344f67cce956d5b6b45d8005060f037925bb6ad
leon0113 <tahjib.leon399@gmail.com>
2023-02-06 03:38:50 +0600

    search by name

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -3,6 +3,7 @@ import Article from '../Article/Article';
 
 const Countries = () => {
     const [countries, setCountries] = useState([]);
+    const [searchText, setSearchText] = useState("");
     const regions = [
         {
             name: 'Europe'
@@ -35,25 +36,43 @@ const Countries = () => {
             }
         }
         getCountries();
-    }, [])
+    }, []);
+
+    async function searchCountry() {
+        try {
+            const res = await fetch(`https://restcountries.com/v3.1/name/${searchText}`)
+            const data = await res.json()
+            setCountries(data)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    function handleSearchCountry(e) {
+        e.preventDefault()
+        searchCountry()
+    }
+
     return (
         <div>
             {!countries ? (<h1 className='text-gray-900 font-bold uppercase flex items-center justify-center text-center h-screen text-4xl tracking-wide dark:text-white'>Loading...</h1>) : (
                 <section className='container mx-auto p-8'>
                     {/* form  */}
                     <div className='flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8'>
-                        <form autoComplete='off' className='max-w-4xl md:first-letter md:flex-1'>
+                        <form onSubmit={handleSearchCountry} autoComplete='off' className='max-w-4xl md:first-letter md:flex-1'>
                             <input type="text"
                                 name='search'
                                 id='search'
                                 placeholder='Search for a country by its name'
                                 required
-                                className='py-3 px-4 text-gray-800 placeholder-gray-800 shadow rounded w-full outline-none'
+                                value={searchText}
+                                onChange={(e) => setSearchText(e.target.value)}
+                                className='py-3 px-4 text-gray-600 placeholder-gray-600 shadow rounded w-full outline-none dark:text-gray-300 dark:placeholder-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700 transition-all duration-200'
                             />
                         </form>
 
                         <form>
-                            <select className='w-52 outline-none py-3 px-4 shadow rounded' name="filter-by-region" id="filter-by-region">
+                            <select className='w-52 outline-none py-3 px-4 shadow rounded text-gray-600 dark:text-gray-400 dark:bg-gray-800 dark:focus:bg-gray-700' name="filter-by-region" id="filter-by-region">
                                 {
                                     regions.map((region, index) => (
                                         <option key={index} value={region.name}>
