2585336006aa8140721285140c94dadf14733552
leon0113 <tahjib.leon399@gmail.com>
2023-02-06 03:11:31 +0600

    form style

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -21,13 +21,19 @@ const Countries = () => {
             {!countries ? (<h1 className='text-gray-900 font-bold uppercase flex items-center justify-center text-center h-screen text-4xl tracking-wide dark:text-white'>Loading...</h1>) : (
                 <section className='container mx-auto p-8'>
                     {/* form  */}
-                    <div>
-                        <form>
-                            <input type="text" name='search' id='search' placeholder='Search for a country by its name' required />
+                    <div className='flex flex-col gap-4 md:flex-row md:items-center md:justify-between mb-8'>
+                        <form autoComplete='off' className='max-w-4xl md:first-letter md:flex-1'>
+                            <input type="text"
+                                name='search'
+                                id='search'
+                                placeholder='Search for a country by its name'
+                                required
+                                className='py-3 px-4 text-gray-800 placeholder-gray-800 shadow rounded w-full outline-none'
+                            />
                         </form>
 
                         <form>
-                            <select name="filter-by-region" id="filter-by-region">
+                            <select className='w-52 outline-none py-3 px-4 shadow rounded' name="filter-by-region" id="filter-by-region">
 
                             </select>
                         </form>
